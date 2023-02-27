ce8dfd3b7836fd2dcda5e904ded3326800a67934
userozay <userozay@icloud.com>
2023-02-27 12:50:17 +0300

    listview_builder

---
+++ lib/scheme_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UIColors {
+  static const PRIMARY_COLOR = Color.fromRGBO(17, 136, 136, 1);
+  static const COLOR_GREY = Colors.grey;
+
+  static const primaryColor = Colors.white;
+  static const secondaryColor = Colors.white;
+
+  static const bgColor = Colors.white;
+
+  static const defaultPadding = 16.0;
+}
